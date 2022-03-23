df3027477bcce0ae393cb46eb7214bbb99971ff8
Elias Santiago Cáceres Melgarejo <78571779+elias7991@users.noreply.github.com>
2022-03-23 00:26:23 -0300

    Update autenticacion.py

--- autenticacion.py
+++ autenticacion.py
@@ -7,10 +7,10 @@ import tweepy as tw
 """
 
 """*************************************VARIABLES DE AUTENTICACION*************************************"""
-consumer_key = "H2cC7eKrsCHqBoBZG6XYNeFAi"
-consumer_secret = "z2d9Riqn3JyeOvkQ0R0E6P89Huo22SEB92GdwAbK69SsS4zt3i"
-access_token = "1443289288817483787-Y23CyxwSuGhXX0M6yZzGKbmsvqNMQV"
-access_token_secret = "18nVpC79FpzY46PrkAQiB9gbXCT7XTRvoL1bJQQbtXIDr"
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
 """************************************AUTENTICACION***************************************************"""
 auth = tw.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
